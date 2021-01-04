d1fa89ac8c8ec22fc8f7830e8e04f8de40433e30
cunwn777 <cunwin@ca.ibm.com>
2021-01-04 13:55:16 -0500

    third time lucky

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as reportWebVitals from './reportWebVitals';
+
+React.createElement()
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+console.log('I am so done');
